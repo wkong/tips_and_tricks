c233e493e4ebd15fe39e0de5e2142adc20bb7403
Simas <the.inn3r@gmail.com>
2011-09-28 12:21:04 +0300

    onClick event, cross-browser event utility, pure JS

---
+++ js_onclick_eventUtility/eventUtility.js
@@ -0,0 +1,48 @@
+
+	var eventUtility = {
+		
+		addEvent : (function() {
+			
+			if (typeof addEventListener !== "undefined") {
+				
+				return function(obj, evt, fn) {
+					
+					obj.addEventListener(evt, fn, false);
+					
+				};
+				
+			} else {
+				
+				return function(obj, evt, fn) {
+					
+					obj.attachEvent("on" + evt, fn);
+					
+				};
+				
+			}
+			
+		}()),
+		
+		removeEvent : (function() {
+			
+			if (typeof removeEventListener !== "undefined") {
+				
+				return function(obj, evt, fn) {
+					
+					obj.removeEventListener(evt, fn, false);
+					
+				};
+				
+			} else {
+				
+				return function(obj, evt, fn) {
+					
+					obj.detachEvent("on" + evt, fn);
+					
+				};
+				
+			}
+			
+		}())
+		
+	};
